860fca5e2d50a3151ede0ba76f58b11febf1a2bd
Fabrizio <fabrizio.nastri@flexup.org>
2023-09-26 00:18:28 +0200

    dynamic breadcrumb

--- assets/README.fab.md
+++
@@ -1,3 +0,0 @@
-
-[Does not seem to work with pnpm](https://github.com/BayBreezy/nuxt3-vuetify3-starter/issues/4) → see the issue on GitHub.
-
--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -1,16 +1,15 @@
 <template>
-  <v-breadcrumbs :items="items">
+  <v-breadcrumbs :items="items" color="grey-lighten-1">
     <template v-slot:prepend>
-      <v-icon size="small" icon="mdi-home"></v-icon>
-    </template>
-    <template v-slot:divider>
-      <v-icon icon="mdi-chevron-right"> </v-icon>
+      <v-icon size="small" icon="mdi-home" color="grey-lighten-1"></v-icon>
     </template>
 
-    <template v-slot:item="{ item }">
-      <v-breadcrumbs-item :href="item.href" :disabled="item.disabled">
-        {{ item.text }}
-      </v-breadcrumbs-item>
+    <template v-slot:divider>
+      <v-icon icon="mdi-chevron-right" color="grey-lighten-1"> </v-icon>
+    </template>
+
+    <template v-slot:item="{ item }" color="grey-lighten-1">
+      <v-breadcrumbs-item :to="item.href" :disabled="item.disabled"> {{ item.text }} </v-breadcrumbs-item>
     </template>
   </v-breadcrumbs>
 </template>
@@ -20,14 +19,16 @@
   import { useRoute } from 'vue-router'
 
   const route = useRoute()
-  const breadcrumbItems = ref([])
+  const items = ref([])
 
   watch(
     route,
     () => {
+      console.log('route:', route)
       const pathSegments = route.path.split('/').filter(segment => segment)
+      console.log('pathSegments:', pathSegments)
 
-      breadcrumbItems.value = pathSegments.map((segment, index, array) => {
+      items.value = pathSegments.map((segment, index, array) => {
         return {
           text: segment.charAt(0).toUpperCase() + segment.slice(1),
           disabled: index === array.length - 1,
@@ -36,7 +37,7 @@
       })
 
       // Prepend the home item
-      breadcrumbItems.value.unshift({
+      items.value.unshift({
         text: 'Home',
         disabled: false,
         href: '/'
