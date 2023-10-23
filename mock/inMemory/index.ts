2b48aa8a9a301622a74268a81a4cc5e6a16f2bcc
Fabrizio <fabrizio.nastri@flexup.org>
2023-10-23 14:27:11 +0200

    automatically reset inMemory index

--- mock/inMemory/index.ts
+++ mock/inMemory/index.ts
@@ -1,45 +1,44 @@
-export * from './account'
-export * from './accountUser'
-export * from './allocation'
-export * from './balance'
-export * from './commitment'
-export * from './contract'
-export * from './country'
-export * from './currency'
-export * from './grouping'
-export * from './groupingAccount'
-export * from './identifier'
-export * from './individual'
-export * from './item'
-export * from './legalPerson'
-export * from './order'
-export * from './organization'
-export * from './resolution'
-export * from './riskFactor'
-export * from './tranche'
-export * from './user'
-export * from './userPassword'
 
-import { accountDatas } from './account'
-import { accountUserDatas } from './accountUser'
-import { countryDatas } from './country'
-import { currencyDatas } from './currency'
-import { groupingAccountDatas } from './groupingAccount'
-import { groupingDatas } from './grouping'
-import { identifierDatas } from './identifier'
-import { individualDatas } from './individual'
-import { itemDatas } from './item'
-import { legalPersons } from './legalPerson'
-import { orderDatas } from './order'
-import { organizationDatas } from './organization'
-import { trancheDatas } from './tranche'
-import { userDatas } from './user'
-import { userPasswordDatas } from './userPassword'
+import { accountDatas } from './account';
+import { accountUserDatas } from './accountUser';
+import { allocationDatas } from './allocation';
+import { countryDatas } from './country';
+import { currencyDatas } from './currency';
+import { groupingDatas } from './grouping';
+import { groupingAccountDatas } from './groupingAccount';
+import { identifierDatas } from './identifier';
+import { individualDatas } from './individual';
+import { itemDatas } from './item';
+import { legalPersonDatas } from './legalPerson';
+import { orderDatas } from './order';
+import { organizationDatas } from './organization';
+import { trancheDatas } from './tranche';
+import { userDatas } from './user';
+import { userPasswordDatas } from './userPassword';
 
-// Important : the keys of inMemory is used to generate the EntityName type
+
+export * from './account';
+export * from './accountUser';
+export * from './allocation';
+export * from './country';
+export * from './currency';
+export * from './grouping';
+export * from './groupingAccount';
+export * from './identifier';
+export * from './individual';
+export * from './item';
+export * from './legalPerson';
+export * from './order';
+export * from './organization';
+export * from './tranche';
+export * from './user';
+export * from './userPassword';
+
+
 export const inMemory = {
   account: accountDatas,
   accountUser: accountUserDatas,
+  allocation: allocationDatas,
   country: countryDatas,
   currency: currencyDatas,
   grouping: groupingDatas,
@@ -47,12 +46,13 @@ export const inMemory = {
   identifier: identifierDatas,
   individual: individualDatas,
   item: itemDatas,
-  legalPerson: legalPersons,
+  legalPerson: legalPersonDatas,
   order: orderDatas,
   organization: organizationDatas,
   tranche: trancheDatas,
   user: userDatas,
-  userPassword: userPasswordDatas
-}
+  userPassword: userPasswordDatas,
 
-export default inMemory
+};
+
+export default inMemory;
---
+++ mock/resetInMemoryIndex.ts
@@ -0,0 +1,42 @@
+import fs from 'fs'
+import path from 'path'
+
+const indexFile = 'index.ts'
+const targetDir = 'mock/inMemory'
+
+// Get all .ts files in the directory except index.ts
+const files = fs.readdirSync(targetDir).filter((file) => file.endsWith('.ts') && file !== indexFile)
+
+let exportStatements = ''
+let importStatements = ''
+let inMemoryObjectEntries = ''
+
+files.forEach((file) => {
+  const moduleName = path.basename(file, '.ts') // Remove the .ts extension
+
+  // check if the file contains an export object named ${moduleName}Datas
+  const fileContent = fs.readFileSync(path.join(targetDir, file), 'utf8')
+  if (!fileContent.includes(`export const ${moduleName}Datas`)) {
+    console.error(`File ${file} does not contain an export object named ${moduleName}Datas`)
+    // skip this file
+    return // equivalent to continue
+  }
+
+  exportStatements += `export * from './${moduleName}';\n`
+  importStatements += `import { ${moduleName}Datas } from './${moduleName}';\n`
+  inMemoryObjectEntries += `  ${moduleName}: ${moduleName}Datas,\n`
+})
+
+const output = `
+${importStatements}
+
+${exportStatements}
+
+export const inMemory = {
+${inMemoryObjectEntries}
+};
+
+export default inMemory;
+`
+
+fs.writeFileSync(path.join(targetDir, indexFile), output)
