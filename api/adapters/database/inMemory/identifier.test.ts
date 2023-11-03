41e7e234bd1cc7082c13bef77be5688af2e8bb47
Fabrizio <fabrizio.nastri@flexup.org>
2023-11-03 18:02:13 +0100

    fixed identifier test

--- api/adapters/database/inMemory/identifier.test.ts
+++ api/adapters/database/inMemory/identifier.test.ts
@@ -1,15 +1,15 @@
 import { identifierAdapter } from './identifier'
-import * as identifiers from 'mock/inMemory'
+import { totoUsernameIdentifierData } from 'mock/inMemory'
 
 describe('identifierAdapter', () => {
   describe('getUserId', () => {
     it('should return userId for valid identifier ', async () => {
-      const result = await identifierAdapter.getUserId('totoUsername')
-      const expected = identifiers.totoUsernameIdentifierData.userId
+      const result = await identifierAdapter.getUserId('totousername')
+      const expected = totoUsernameIdentifierData.userId
       expect(result).toEqual(expected)
     })
     it('should throw an error for invalid identifier', async () => {
-      await expect(identifierAdapter.getUserId('plop')).rejects.toThrowError('Invalid identifier')
+      await expect(() => identifierAdapter.getUserId('plop')).toThrowError('Invalid identifier')
     })
   })
 })
