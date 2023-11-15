a5b8ff79b0faf61df393cf3ccb3775bf1139ef53
Fabrizio <fabrizio.nastri@flexup.org>
2023-11-15 14:27:39 +0100

    finished orders table and composable

---
+++ app/components/OrderTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="relative overflow-x-auto">
+    <table class="w-full text-left text-sm text-gray-500">
+      <thead class="bg-gray-50 text-xs uppercase text-gray-700">
+        <tr>
+          <th scope="col" class="px-6 py-3">Order ID</th>
+          <th scope="col" class="px-6 py-3">Date</th>
+          <th scope="col" class="px-6 py-3">Status</th>
+          <th scope="col" class="px-6 py-3">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="order in orders" :key="order.id" class="border-b bg-white">
+          <td class="px-6 py-4">{{ order.id }}</td>
+          <td class="px-6 py-4">{{ order.date }}</td>
+          <td class="px-6 py-4">{{ order.status }}</td>
+          <td class="px-6 py-4">{{ order.total }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+  import { onMounted, ref } from 'vue'
+  import { useOrderStore } from '@/stores/useOrderStore'
+  import { useUserStore } from '@/stores/useUserStore'
+  import { useAccountStore } from '@/stores/useAccountStore'
+</script>
+
+<style scoped>
+  /* Add component-specific styles here */
+</style>
--- app/pages/orders.vue
+++ app/pages/orders.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="container mx-auto p-6">
-    <h1 class="mb-2 text-xl font-bold">Orders</h1>
-    <p class="mb-4 text-gray-600">List of all orders in the current account.</p>
-    <!-- <OrderTable /> -->
+    <div class="sm:flex sm:items-center">
+      <div class="sm:flex-auto">
+        <h1>Orders</h1>
+        <p class="mt-2 text-sm text-gray-700">List of all orders</p>
+      </div>
+      <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+        <button
+          type="button"
+          class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          + New Order
+        </button>
+      </div>
+    </div>
+
+    <OrderTable2 />
+    <!-- <div class="my-20"></div>
+    <hr />
+    <div class="my-20"></div>
+    <OrderTable /> -->
   </div>
 </template>
